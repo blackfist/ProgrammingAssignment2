bbe8da91970198d7145180885d701dd8e4c86636
Kevin Thompson <v527234@TUSMN003MLTE001.local>
2014-06-07 21:26:15 -0500

    creates a working makeCacheMatrix class

--- cachematrix.R
+++ cachematrix.R
@@ -2,8 +2,29 @@
 ## functions do
 
 ## Write a short comment describing this function
+# The makeCacheMatrix function will store the inverse of the 
+# given matrix in a variable called inverseMatrix.
 
 makeCacheMatrix <- function(x = matrix()) {
+  inverseMatrix <- NULL
+  
+  # When the get function is invoked, the cached value is returned
+  # which may be NULL if the inverse has not yet been calculated
+  set <- function(y) {
+    unInvertedMatrix <<- y
+    inverseMatrix <<- NULL
+  }
+  
+  get <- function() unInvertedMatrix
+  
+  getInverse <- function() inverseMatrix 
+  
+  setInverse <- function(inverse) inverseMatrix <<- inverse
+  
+  # I still don't know what this part does exactly, but without it you
+  # get errors: object of type 'closure' is not subsettable
+  # this might be what exports the sub functions of the function
+  list(get=get, set=set, getInverse=getInverse, setInverse=setInverse)
 
 }
 
